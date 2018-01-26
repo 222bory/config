3bb15942c62d7d8442eaeee95f8af1d94a75d6d4
Leonardo Park <ageofblue@gmail.com>
2018-01-26 16:56:43 +0900

    Jenkins test

--- src/main/java/com/ktds/msa/config/Application.java
+++ src/main/java/com/ktds/msa/config/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
 
+
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
